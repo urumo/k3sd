a0a306043d44a1ae69d917c3fa0a146d0fe5f3eb
Aram <aramhrptn@hotmail.com>
2025-05-18 03:06:16 +0400

    add flag parsing functionality for cluster management

---
+++ utils/flags.go
@@ -0,0 +1,39 @@
+package utils
+
+import (
+	"flag"
+	"log"
+)
+
+var (
+	Flags      map[string]bool
+	ConfigPath string
+	Uninstall  bool
+)
+
+func ParseFlags() {
+	certManager := flag.Bool("cert-manager", false, "Apply the cert-manager YAMLs")
+	traefik := flag.Bool("traefik", false, "Apply the Traefik YAML")
+	clusterIssuer := flag.Bool("cluster-issuer", false, "Apply the Cluster Issuer YAML")
+	gitea := flag.Bool("gitea", false, "Apply the Gitea YAML")
+	configPath := flag.String("config-path", "", "Path to clusters.json")
+	prometheus := flag.Bool("prometheus", false, "Apply the Prometheus YAML")
+	uninstallFlag := flag.Bool("uninstall", false, "Uninstall the cluster")
+
+	flag.Parse()
+
+	Uninstall = *uninstallFlag
+	Flags = map[string]bool{
+		"cert-manager":   *certManager,
+		"traefik-values": *traefik,
+		"clusterissuer":  *clusterIssuer,
+		"gitea":          *gitea,
+		"prometheus":     *prometheus,
+	}
+
+	if *configPath != "" {
+		ConfigPath = *configPath
+	} else {
+		log.Fatalf("Must specify --config-path")
+	}
+}
