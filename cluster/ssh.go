ec90dfc37ef57b71f05c35fc017e577bfcd700e2
Aram <aramhrptn@hotmail.com>
2025-05-19 03:48:15 +0400

    refactor ExecuteCommands to streamline command execution and enhance output handling

--- cluster/ssh.go
+++ cluster/ssh.go
@@ -11,32 +11,28 @@ import (
 
 func ExecuteCommands(client *ssh.Client, commands []string) error {
 	for _, cmd := range commands {
-		session, err := client.NewSession()
-		if err != nil {
-			return fmt.Errorf("failed to create session: %v", err)
-		}
-		defer session.Close()
-
-		stdoutPipe, err := session.StdoutPipe()
-		if err != nil {
-			return fmt.Errorf("failed to get stdout pipe: %v", err)
-		}
-
-		stderrPipe, err := session.StderrPipe()
-		if err != nil {
-			return fmt.Errorf("failed to get stderr pipe: %v", err)
-		}
-
-		go streamOutput(stdoutPipe, false)
-		go streamOutput(stderrPipe, true)
-
-		utils.Log("Running command: %s", cmd)
-
-		if err := session.Run(cmd); err != nil {
-			return fmt.Errorf("command failed: %v", err)
+		if err := runCommand(client, cmd); err != nil {
+			return err
 		}
 	}
 	return nil
+}
+
+func runCommand(client *ssh.Client, cmd string) error {
+	session, err := client.NewSession()
+	if err != nil {
+		return fmt.Errorf("failed to create session: %v", err)
+	}
+	defer session.Close()
+
+	stdout, _ := session.StdoutPipe()
+	stderr, _ := session.StderrPipe()
+
+	go streamOutput(stdout, false)
+	go streamOutput(stderr, true)
+
+	utils.Log("Running command: %s", cmd)
+	return session.Run(cmd)
 }
 
 func streamOutput(r io.Reader, isErr bool) {
@@ -62,8 +58,7 @@ func ExecuteRemoteScript(client *ssh.Client, script string) (string, error) {
 	session.Stdout = &stdout
 	session.Stderr = &stderr
 
-	cmd := fmt.Sprintf("bash -c '%s'", script)
-	if err := session.Run(cmd); err != nil {
+	if err := session.Run(fmt.Sprintf("bash -c '%s'", script)); err != nil {
 		return "", fmt.Errorf("error executing script: %v, stderr: %s", err, stderr.String())
 	}
 
