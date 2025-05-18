5f16a8c2f5c4f4e7119ad3be2fb28accd6e2133d
Aram <aramhrptn@hotmail.com>
2025-05-19 03:51:37 +0400

    document LogChannel and Log functions for clarity and usage details

--- utils/logger.go
+++ utils/logger.go
@@ -5,17 +5,23 @@ import (
 	"log"
 )
 
-var LogChannel chan string = make(chan string, 100)
+// LogChannel is a buffered channel used to queue log messages for processing.
+var LogChannel = make(chan string, 100)
 
+// Log formats a log message and sends it to the LogChannel for processing.
+//
+// Parameters:
+//   - format: A string containing the format of the log message (similar to fmt.Sprintf).
+//   - args: A variadic list of arguments to be formatted into the log message.
 func Log(format string, args ...interface{}) {
 	LogChannel <- fmt.Sprintf(format, args...)
 }
 
+// LogWorker continuously processes log messages from the LogChannel and writes them to the standard logger.
+//
+// This function should be run as a goroutine to handle log messages asynchronously.
 func LogWorker() {
-	for {
-		select {
-		case logMessage := <-LogChannel:
-			log.Println(logMessage)
-		}
+	for logMessage := range LogChannel {
+		log.Println(logMessage)
 	}
 }
